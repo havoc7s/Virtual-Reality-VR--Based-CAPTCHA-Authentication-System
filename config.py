bed51f6a841a8d07f1a886aea253412198d48ba5
Salamah Obeidat <salamahobeidat7@gmail.com>
2026-06-19 21:34:03 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+db_config = {
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT"))
+}
